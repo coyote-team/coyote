8bbe916a978b375c9f274b67414b3202a94189ce
Christopher Reed <mail@seeread.info>
2015-07-08 15:06:11 -0400

    bootstrap classes for alert error and warning

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,8 +30,8 @@ module ApplicationHelper
     case level.to_sym
       when :notice then "alert alert-info alert-dismissable"
       when :success then "alert alert-success alert-dismissable"
-      when :error then "alert alert-error alert-dismissable"
-      when :alert then "alert alert-error alert-dismissable"
+      when :error then "alert alert-warning alert-dismissable"
+      when :alert then "alert alert-danger alert-dismissable"
     end
   end
 
