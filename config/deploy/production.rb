b276f777bf8d4786988e85beb2153f750d57199d
Christopher Reed <mail@seeread.info>
2015-08-17 16:00:12 -0400

    ip not domain for deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :stage, :production
-server 'coyote.mcachicago.org', user: 'seeread', roles: %w{web app db}
+server '104.154.48.143', user: 'seeread', roles: %w{web app db}
 set :linked_files, %w{.env .env.production} #TODO make this smarter
