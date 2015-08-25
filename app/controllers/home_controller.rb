c2e82b886b45cca4faf1c74fc894d1cf05de902c
Christopher Reed <mail@seeread.info>
2015-08-25 09:55:35 -0400

    faster home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,18 +6,18 @@ class HomeController < ApplicationController
       #TODO should only show my content descriptions for these two
       limit = 10
       @my_queue = current_user.images.collect{|i| i if i.undescribed_by?(current_user)}.compact.first(limit)
-      @my_described_queue = current_user.images.collect{|i| i if i.described_by?(current_user)}.compact.first(limit)
-      @my_completed_queue= current_user.images.collect{|i| i if i.completed_by?(current_user)}.compact.first(limit)
+      #@my_described_queue = current_user.images.collect{|i| i if i.described_by?(current_user)}.compact.first(limit)
+      #@my_completed_queue= current_user.images.collect{|i| i if i.completed_by?(current_user)}.compact.first(limit)
       @my_description_ids = current_user.descriptions.map{|d| d.id}
 
       if current_user.admin?
         images = Image.all
         @unassigned = images.unassigned.first(limit)
-        @assigned = images.assigned.first(limit)
-        @completed = @assigned.collect{|i| i if i.completed?}.compact.first(limit)
-        @incomplete = @assigned.collect{|i| i unless i.completed?}.compact.first(limit)
-        @ready_to_review = @assigned.collect{|i| i if i.ready_to_review?}.compact.first(limit)
-        @not_approved = @assigned.collect{|i| i if i.not_approved?}.compact.first(limit)
+        #@assigned = images.assigned.first(limit)
+        #@completed = images.collect{|i| i if i.completed?}.compact.first(limit)
+        #@incomplete = images.collect{|i| i unless i.completed?}.compact.first(limit)
+        @ready_to_review = Description.ready_to_review.collect{|d| d.image}.uniq.first(10)
+        #@not_approved = images.collect{|i| i if i.not_approved?}.compact.first(limit)
       end
     end
   end
