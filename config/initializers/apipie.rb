715bc9cca7392a17f2e4eeb6d7404b62738b8eed
Christopher Reed <mail@seeread.info>
2016-10-24 22:37:20 -0400

    clearer instructions for API usage

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -7,11 +7,14 @@ Apipie.configure do |config|
   config.app_info = <<-EOT
 Image annotation site and API to enable the distributed annotation of museum images.  
 
-All API endppoints expect <code>.json</code> as an ending.  You can hit the endpoints when you have logged in.  Otherwise, you can find your user token in the regular site footer after you have logged in and then you can use request headers:
+All API endppoints expect either a json content type header or <code>.json</code> as an ending. You can find your user token in the regular site footer after you have logged in and then you can use either a request header like so:
+
     X-User-Email alice@example.com
     X-User-Token 1G8_s7P-V-4MGojaKD7a
+
 Or a query string to authenticate:
-   GET http://coyote.mcachicago.org?user_email=alice@example.com&user_token=1G8_s7P-V-4MGojaKD7a
+
+   GET http://coyote.mcachicago.org/websites.json?user_token=1G8_s7P-V-4MGojaKD7a
 
 When a description is approved for an image, a PATCH request is sent out to its resource URI.
   EOT
