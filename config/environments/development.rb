d4f5242476fea9138c3b2215f46548eaf7f2d5c3
Christopher Reed <mail@seeread.info>
2016-10-27 17:22:04 -0400

    development mail testing easier

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,9 @@ Plate::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -28,7 +29,6 @@ Plate::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   config.assets.raise_production_errors = true
-
 
   ##SPROCKETS DEBUGGING
   #config.assets.debug = false
