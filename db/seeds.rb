f5775c23cf58dece721e07f104b3fb9720688a1b
Christopher Reed <mail@seeread.info>
2016-10-26 20:42:49 -0400

    text updates close #61

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@ Group.create!([
   {id: 4, title: "events "},
 ])
 Metum.create!([
-  {id: 1, title: "Alt", instructions: "Alt, or alternative, text is the text that will be read to a blind user by their screen reader. If the image is a UI element, one to two words is preferred e.g. home, Twitter, Contact Us, etc. If the image is not a UI element, then the alt text should describe the image’s main purpose or theme. It’s best to keep the alt text less than 20 to 30 words. Long description exists for longer form description. If this image has a caption, then try not to repeat caption text in this alt text."},
+  {id: 1, title: "Alt", instructions: "A long description is a lengthier text than a traditional alt-text that attempts to provide a . comprehensive representation of an image. Long descriptions can range from one sentence to several paragraphs."},
   #{id: 2, title: "Caption", instructions: "This is the caption text to be displayed visibly. This text will also be read to a blind user so it is best not to repeat info from this item in the alt text."},
-  {id: 3, title: "Long", instructions: "A long description is a lengthier description of an image compared to the alternative text. It is designed with description, instead of function, in mind. see description guidelines for specific guidance for aesthetics."}
+  {id: 3, title: "Long", instructions: "A long description is a lengthier text than a traditional alt-text that attempts to provide a . comprehensive representation of an image. Long descriptions can range from one sentence to several paragraphs."}
 ])
 Status.create!([
   {id: 1, title: "Ready to review"},
