f5c8106e8a33bc9db2b55950462653d2d96cdadb
Christopher Reed <mail@seeread.info>
2017-04-05 14:52:18 -0400

    secret key generator in conf script ref #79

--- bin/conf_creator.sh
+++ bin/conf_creator.sh
@@ -11,7 +11,7 @@ function populate {
     echo "#Created ${TIMESTAMP}" > $FILE
     while read -r NAME VALUE
     do
-        if [ ${#NAME} = "SECRET_KEY_BASE" ]
+        if [ $NAME = "SECRET_KEY_BASE" ]
         then
             SECRET="$(bin/rake secret)"
             echo "$NAME=$SECRET" >> $FILE
