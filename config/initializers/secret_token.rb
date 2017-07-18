84e3f328a62a8df43a359a2f2068df689a95a42b
Mike Subelsky <mike@subelsky.com>
2017-07-18 09:37:46 -0400

    Initial effort to overhaul ENV configuration; this brings us up to current Rails standards and should help with future deployment to cloud systems

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,3 @@
-Plate::Application.config.secret_key_base  = ENV['SECRET_KEY_BASE']
+Plate::Application.config.secret_key_base  = ENV.fetch("SECRET_KEY_BASE") do 
+  abort("Please set ENV variable SECRET_KEY_BASE (see .env file)") 
+end
