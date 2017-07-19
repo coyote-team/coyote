ee503f74c4b6fde1c23ac3905d8b4ea6c592e302
Mike Subelsky <mike@subelsky.com>
2017-07-18 22:39:18 -0400

    Mount rspec with .rspec so we don’t have to use rake task [#106]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,14 +3,15 @@ require 'rspec/rails'
 require 'webmock/rspec'
 require 'capybara/rspec'
 require "codeclimate-test-reporter"
+
 CodeClimate::TestReporter.start
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |file| require file }
 
 RSpec.configure do |config|
-  #http://dchua.com/2013/08/19/dry-up-your-devise-logins-in-rspec/
   config.include Devise::TestHelpers, type: :controller
   config.include ControllerMacros, :type => :controller
+  config.include FactoryGirl::Syntax::Methods
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -22,14 +23,12 @@ RSpec.configure do |config|
   end
 
   config.fail_fast = true
-  #config.fail_fast = false
   config.infer_spec_type_from_file_location!
   config.infer_base_class_for_anonymous_controllers = false
   config.order = 'random'
   config.use_transactional_fixtures = false
 end
 
-#Test coverage
 SimpleCov.start do
   add_filter "/config/" # Ignores any file containing "/config/" in its path.
 end
