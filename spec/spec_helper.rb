e725316c7dd02bffaed904970406b5556116b41f
Mike Subelsky <mike@subelsky.com>
2017-07-20 10:37:12 -0400

    disable RSpec monkey patching to ensure a brighter, minimally-metaprogrammed future

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,7 +30,7 @@ RSpec.configure do |config|
 
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
-  #config.disable_monkey_patching! 
+  config.disable_monkey_patching! 
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
