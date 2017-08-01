19b2f01a1af876c9a6b91507bf87c4a8dfe4b805
Mike Subelsky <mike@subelsky.com>
2017-08-01 14:44:01 -0400

    Create test stub for viewing support page - good place for a beginner to start

---
+++ spec/features/viewing_support_page_spec.rb
@@ -0,0 +1,8 @@
+RSpec.feature "Viewing support page" do
+  include_context "as a logged-in viewer user"
+
+  scenario "succeeds" do
+    skip "this would be great for a volunteer to work on, good way to learn Capybara"
+    # This needs to exercise clicking on the "Support" link in the upper right hand navigation menu
+  end
+end
