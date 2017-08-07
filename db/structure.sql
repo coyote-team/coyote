2a95c503c388569b027d3817c89063c60b673fb7
Mike Subelsky <mike@subelsky.com>
2017-08-07 10:55:46 -0400

    remove offending statement from db structure to appease travis

--- db/structure.sql
+++ db/structure.sql
@@ -6,7 +6,7 @@
 -- Dumped by pg_dump version 9.6.3
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
+-- SET lock_timeout = 0; # causes build failure
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
