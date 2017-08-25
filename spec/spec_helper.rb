9ee6e6fa52a1462367faaf176333a1a67e1810e5
Mike Subelsky <mike@subelsky.com>
2017-08-25 10:59:42 -0400

    Explicitly require database_cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,15 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?  # Extra check to prevent database changes if the environment is production
 
-require 'rspec/rails'
-require 'factory_girl_rails'
-require 'devise'
-require 'webmock/rspec'
-require 'capybara/rspec'
-require 'simplecov'
-require 'airborne'
-require 'vcr'
+require "rspec/rails"
+require "factory_girl_rails"
+require "devise"
+require "webmock/rspec"
+require "capybara/rspec"
+require "simplecov"
+require "airborne"
+require "vcr"
 require "pathname"
+require "database_cleaner"
 
 SPEC_DATA_PATH = Pathname(__dir__).join("data")
 
