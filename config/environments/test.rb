d6262ae2acc8f223ea10881c00076b55bc344558
Mike Subelsky <mike@subelsky.com>
2017-08-28 16:05:24 -0400

    Feature test resetting a password #120

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,6 +34,8 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  config.action_mailer.default_url_options = { host: 'coyote.example' }
+  
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
---
+++ spec/features/resetting_password_spec.rb
@@ -0,0 +1,19 @@
+RSpec.feature "Resetting a password" do
+  let(:user) { create(:user) }
+
+  scenario "succeeds", :focus do
+    visit new_user_password_path
+
+    fill_in "user[email]", with: user.email
+
+    expect {
+      click_button "Send me reset password instructions"
+    }.to change(ActionMailer::Base.deliveries,:count).
+      from(0).to(1)
+
+    ActionMailer::Base.deliveries.first.tap do |email|
+      expect(email.to).to eq([user.email])
+      expect(email.subject).to match(/reset password/i)
+    end
+  end
+end
