b19ac4677aebd25942710a8d8e03c5fd91252a8d
Mike Subelsky <mike@subelsky.com>
2017-08-28 15:56:33 -0400

    Set default actionmailer host so reset password works #120

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
+  config.action_mailer.default_url_options = { host: ENV.fetch('HOST','coyote.example') }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
