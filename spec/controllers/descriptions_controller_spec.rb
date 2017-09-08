bc320edeebcd158dfc00000ae9a07e34607fae4e
Mike Subelsky <mike@subelsky.com>
2017-09-08 15:26:09 -0400

    Minor cleanups while writing invitation code #111

--- spec/controllers/descriptions_controller_spec.rb
+++ spec/controllers/descriptions_controller_spec.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: descriptions
+#
+#  id         :integer          not null, primary key
+#  locale     :string           default("en")
+#  text       :text
+#  status_id  :integer          not null
+#  image_id   :integer          not null
+#  metum_id   :integer          not null
+#  user_id    :integer          not null
+#  created_at :datetime
+#  updated_at :datetime
+#  license    :string           default("cc0-1.0")
+#
+# Indexes
+#
+#  index_descriptions_on_image_id   (image_id)
+#  index_descriptions_on_metum_id   (metum_id)
+#  index_descriptions_on_status_id  (status_id)
+#  index_descriptions_on_user_id    (user_id)
+#
+
 RSpec.describe DescriptionsController do
   let(:organization) { create(:organization) }
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,3 +1,32 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  first_name             :string
+#  last_name              :string
+#  authentication_token   :string
+#  staff                  :boolean          default(FALSE), not null
+#
+# Indexes
+#
+#  index_users_on_authentication_token  (authentication_token)
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
 RSpec.describe UsersController do
   let(:organization) { create(:organization) }
 
