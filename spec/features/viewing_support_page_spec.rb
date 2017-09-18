56f7c9b4896f9d088b6b1af9ab3b0440a2419e78
Mike Subelsky <mike@subelsky.com>
2017-09-18 13:32:01 -0400

    Remove unneeded support page view spec

--- spec/features/viewing_support_page_spec.rb
+++
@@ -1,8 +0,0 @@
-RSpec.feature "Viewing support page" do
-  include_context "as a logged-in viewer user"
-
-  scenario "succeeds" do
-    skip "this would be great for a volunteer to work on, good way to learn Capybara"
-    # This needs to exercise clicking on the "Support" link in the upper right hand navigation menu
-  end
-end
