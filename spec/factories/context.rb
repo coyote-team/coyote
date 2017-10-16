e280e22370a69f47aefc923c6358b3b8b3a0042c
Mike Subelsky <mike@subelsky.com>
2017-10-16 09:43:37 -0400

    More realistic types for Contexts factory

--- spec/factories/context.rb
+++ spec/factories/context.rb
@@ -12,5 +12,11 @@ FactoryGirl.define do
   factory :context do
     title { Faker::Lorem.unique.word }
     organization
+
+    %i[collection website exhibitions events].each do |trait_name|
+      trait trait_name do
+        title trait_name
+      end
+    end
   end
 end
