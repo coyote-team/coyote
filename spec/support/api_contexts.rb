d30acf0e38cef76cbcb5226232d28a57c1e9690d
Mike Subelsky <mike@subelsky.com>
2017-10-16 09:45:00 -0400

    Rename request contexts to API contexts

--- spec/support/api_contexts.rb
+++ spec/support/api_contexts.rb
@@ -8,8 +8,13 @@ Coyote::Membership.each_role do |_,role_name|
       create(:membership,role_name,user: user,organization: user_organization)
     end
 
+    let(:api_headers) do
+      mime_type = Rails.configuration.x.api_mime_type_template % { version: 'v1' }
+      { Accept: mime_type }
+    end
+
     let(:auth_headers) do
-      { Authorization: user.authentication_token }
+      api_headers.merge(Authorization: user.authentication_token)
     end
   end
 end
