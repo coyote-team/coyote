a8e81a00ca0d97f82fb51a5607e6db83b961a2af
Mike Subelsky <mike@subelsky.com>
2017-11-01 17:23:52 -0400

    feat: add scroll-to queue links to dashboard w/ ajax

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -23,4 +23,10 @@ class Organization < ApplicationRecord
   has_many :meta, :inverse_of => :organization
   has_many :assignments, :through => :resources
   has_many :representations, :through => :resources
+
+  has_many :unassigned_unrepresented_resources, ->() { unassigned_unrepresented }, :class_name => :Resource
+
+  def ready_to_review_representations
+    representations.ready_to_review
+  end
 end
