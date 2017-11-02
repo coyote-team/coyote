57cf460fcbab6d12843c7ba2ecc46e40ce324e1f
Mike Subelsky <mike@subelsky.com>
2017-11-02 13:31:02 -0400

    fix: do not report record not found to rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -22,8 +22,8 @@ if ENV['ROLLBAR_ACCESS_TOKEN'].present?
     # via the rollbar interface.
     # Valid levels: 'critical', 'error', 'warning', 'info', 'debug', 'ignore'
     # 'ignore' will cause the exception to not be reported at all.
-    # config.exception_level_filters.merge!('MyCriticalException' => 'critical')
-    #
+    config.exception_level_filters.merge!('ActiveRecord::RecordNotFound' => 'ignore')
+    
     # You can also specify a callable, which will be called with the exception instance.
     # config.exception_level_filters.merge!('MyCriticalException' => lambda { |e| 'critical' })
 
