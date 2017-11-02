00f20b066cdc067ee6bd4ff7ca5a27fa453a215e
Mike Subelsky <mike@subelsky.com>
2017-11-02 13:25:22 -0400

    fix: handle representation status changes when status is blank

--- app/controllers/representation_status_changes_controller.rb
+++ app/controllers/representation_status_changes_controller.rb
@@ -4,17 +4,20 @@ class RepresentationStatusChangesController < ApplicationController
   def create
     status, representation_ids = representation_status_change_params.values_at(:status,:representation_ids)
 
-    representation_ids = Array(representation_ids)
+    update_count = if status.present?
+                     representation_ids = Array(representation_ids)
 
-    representations = current_organization.representations.where(id: representation_ids)
-    representations.each { |r| authorize(r) }
+                     representations = current_organization.representations.where(id: representation_ids)
+                     representations.each { |r| authorize(r) }
 
-    representations.update_all(status: status)
+                     representations.update_all(status: status)
+                     logger.info "Update Representation IDs #{representation_ids.to_sentence} to status '#{status}'"
+                   else
+                     logger.warn 'Did not update any representations since status was blank'
+                     0
+                   end
 
-    logger.info "Update Representation IDs #{representation_ids.to_sentence} to status '#{status}'"
-    flash[:notice] = "Set #{representations.count} #{'representation'.pluralize(representations.count)} to status '#{status.titleize}'"
-
-    redirect_back fallback_location: root_url
+    redirect_back fallback_location: root_url, notice: "Set #{update_count} #{'representation'.pluralize(update_count)} to status '#{status.titleize}'"
   end
 
   private
