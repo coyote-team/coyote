91d7c464e0e44ebe738cd0bf5d9fc2565336c09e
Mike Subelsky <mike@subelsky.com>
2017-11-15 16:38:42 -0500

    chore: reveal Ransack unknown attributes in dev/test env

---
+++ config/initializers/ransack.rb
@@ -0,0 +1,5 @@
+Ransack.configure do |config|
+  # Raise errors in dev and test environments if a query contains an unknown predicate or attribute.
+  # Don't raise them in production
+  config.ignore_unknown_conditions = Rails.env.production?
+end
