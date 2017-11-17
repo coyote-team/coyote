13b24b8c95b15cbe35a98c77d8b8b8f58b69ffac
Mike Subelsky <mike@subelsky.com>
2017-11-17 11:46:13 -0500

    feat: remember user’s original destination after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery :with => :exception
 
+  before_action :store_user_location!, :if => :storable_location? # see https://github.com/plataformatec/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, :if => :devise_controller?
 
@@ -31,7 +32,9 @@ class ApplicationController < ActionController::Base
   alias pundit_user organization_user
 
   def after_sign_in_path_for(user)
-    if user.organizations.one?
+    if (original_path = stored_location_for(:user))
+      original_path
+    elsif user.organizations.one?
       organization_path(user.organizations.first)
     else
       organizations_path
@@ -52,5 +55,15 @@ class ApplicationController < ActionController::Base
 
   def pagination_params
     params.fetch(:page,{}).permit(:number,:size)
+  end
+
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+  end
+
+  def store_user_location!
+    store_location_for(:user,request.fullpath)
   end
 end
--- spec/features/logging_in_and_out_spec.rb
+++ spec/features/logging_in_and_out_spec.rb
@@ -1,18 +1,26 @@
 RSpec.feature 'Logging in and out' do
   let(:password) { 'abc123PDQ' }
 
+  let!(:organization) { create(:organization) }
+
   let!(:user) do 
-    create(:user,:with_membership,password: password) 
+    create(:user,organization: organization,password: password) 
+  end
+
+  let(:resource) do
+    create(:resource,organization: organization)
   end
 
   scenario 'Logging in with correct credentials and logging out' do
+    visit resource_path(resource)
+    expect(page.status_code).to eq(200)
+    expect(page.current_path).to eq(new_user_session_path)
+
     login(user,password)
+
     expect(page.status_code).to eq(200)
     expect(page).to have_content 'Signed in successfully'
-
-    user.organizations.each do |org|
-      expect(page).to have_content(org.title)
-    end
+    expect(page.current_path).to eq(resource_path(resource))
 
     logout
 
