33b40db835012479bd8d3143eddf41669b424250
Flip Sasser <flip@inthebackforty.com>
2018-04-05 13:48:14 -0400

    Fix chip label and add button toggle functionality (incomplete)

--- app/assets/javascripts/vendor/bootstrap-native-v4.js
+++ app/assets/javascripts/vendor/bootstrap-native-v4.js
@@ -9,6 +9,7 @@
   } else {
     // Browser globals (root is window)
     var bsn = factory();
+    root.Button = bsn.Button;
     root.Dropdown = bsn.Dropdown;
   }
 }(this, function () {
@@ -298,6 +299,111 @@
 
   BSN.version = '2.0.23';
 
+  /* Native Javascript for Bootstrap 4 | Button
+  ---------------------------------------------*/
+
+  // BUTTON DEFINITION
+  // ===================
+  var Button = function( element ) {
+
+    // initialization element
+    element = queryElement(element);
+
+    // constant
+    var toggled = false, // toggled makes sure to prevent triggering twice the change.bs.button events
+
+        // strings
+        component = 'button',
+        checked = 'checked',
+        reset = 'reset',
+        LABEL = 'LABEL',
+        INPUT = 'INPUT',
+
+      // private methods
+      keyHandler = function(e){
+        var key = e.which || e.keyCode;
+        key === 32 && e[target] === DOC.activeElement && toggle(e);
+      },
+      preventScroll = function(e){
+        var key = e.which || e.keyCode;
+        key === 32 && e[preventDefault]();
+      },
+      toggle = function(e) {
+        var label = e[target].tagName === LABEL ? e[target] : e[target][parentNode].tagName === LABEL ? e[target][parentNode] : null; // the .btn label
+
+        if ( !label ) return; //react if a label or its immediate child is clicked
+
+        var eventTarget = e[target], // the button itself, the target of the handler function
+          labels = getElementsByClassName(eventTarget[parentNode],'btn'), // all the button group buttons
+          input = label[getElementsByTagName](INPUT)[0];
+
+        if ( !input ) return; //return if no input found
+
+        // manage the dom manipulation
+        if ( input.type === 'checkbox' ) { //checkboxes
+          if ( !input[checked] ) {
+            addClass(label,active);
+            input[getAttribute](checked);
+            input[setAttribute](checked,checked);
+            input[checked] = true;
+          } else {
+            removeClass(label,active);
+            input[getAttribute](checked);
+            input.removeAttribute(checked);
+            input[checked] = false;
+          }
+
+          if (!toggled) { // prevent triggering the event twice
+            toggled = true;
+            bootstrapCustomEvent.call(input, changeEvent, component); //trigger the change for the input
+            bootstrapCustomEvent.call(element, changeEvent, component); //trigger the change for the btn-group
+          }
+        }
+
+        if ( input.type === 'radio' && !toggled ) { // radio buttons
+          if ( !input[checked] ) { // don't trigger if already active
+            addClass(label,active);
+            input[setAttribute](checked,checked);
+            input[checked] = true;
+            bootstrapCustomEvent.call(input, changeEvent, component); //trigger the change for the input
+            bootstrapCustomEvent.call(element, changeEvent, component); //trigger the change for the btn-group
+
+            toggled = true;
+            for (var i = 0, ll = labels[length]; i<ll; i++) {
+              var otherLabel = labels[i], otherInput = otherLabel[getElementsByTagName](INPUT)[0];
+              if ( otherLabel !== label && hasClass(otherLabel,active) )  {
+                removeClass(otherLabel,active);
+                otherInput.removeAttribute(checked);
+                otherInput[checked] = false;
+                bootstrapCustomEvent.call(otherInput, changeEvent, component); // trigger the change
+              }
+            }
+          }
+        }
+        setTimeout( function() { toggled = false; }, 50 );
+      };
+
+    // init
+    if ( !( stringButton in element ) ) { // prevent adding event handlers twice
+      on( element, clickEvent, toggle );
+      queryElement('['+tabindex+']',element) && on( element, keyupEvent, keyHandler ),
+                                                on( element, keydownEvent, preventScroll );
+    }
+
+    // activate items on load
+    var labelsToACtivate = getElementsByClassName(element, 'btn'), lbll = labelsToACtivate[length];
+    for (var i=0; i<lbll; i++) {
+      !hasClass(labelsToACtivate[i],active) && queryElement('input:checked',labelsToACtivate[i])
+                                            && addClass(labelsToACtivate[i],active);
+    }
+    element[stringButton] = this;
+  };
+
+  // BUTTON DATA API
+  // =================
+  supports[push]( [ stringButton, Button, '['+dataToggle+'="buttons"]' ] );
+
+
   /* Native Javascript for Bootstrap 4 | Dropdown
   ----------------------------------------------*/
 
@@ -451,6 +557,7 @@
   DOC[body] ? initCallback() : on( DOC, 'DOMContentLoaded', function(){ initCallback(); } );
 
   return {
+    Button: Button,
     Dropdown: Dropdown
   };
 }));
