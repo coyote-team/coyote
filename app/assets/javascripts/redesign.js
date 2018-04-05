ee9d2549cdc3b3de236aac64bfddc240cf8b5153
Flip Sasser <flip@inthebackforty.com>
2018-04-05 19:00:04 -0400

    More Sina feedback

--- app/assets/javascripts/redesign.js
+++ app/assets/javascripts/redesign.js
@@ -1,4 +1,19 @@
 //= require vendor/bootstrap-native-v4
 
-document.querySelectorAll(".dropdown").forEach(function(container) {
+function SegmentedControl(control) {
+  this.control = control;
+  this.items = this.control.querySelectorAll(".segmented-control-item");
+  this.items.forEach(function(item) {
+    item.addEventListener("click", function(event) {
+      var isPressed = item["aria-pressed"] == "true";
+      this.items.forEach(function(otherItem) {
+        otherItem.setAttribute("aria-pressed", "false");
+      });
+      item.setAttribute("aria-pressed", isPressed ? "false" : "true");
+    }.bind(this));
+  }.bind(this));
+}
+
+document.querySelectorAll(".segmented-control").forEach(function(control) {
+  var segmentedControl = new SegmentedControl(control);
 });
