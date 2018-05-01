397979f664e8c85b8d85935742c4c2f7485819db
Flip Sasser <flip@inthebackforty.com>
2018-05-01 12:06:21 -0400

    Add mosaic grid layout for modern browsers and lists in grid view:

---
+++ app/assets/javascripts/components/List.js
@@ -0,0 +1,44 @@
+function List(container) {
+  // Stash the control and its items
+  this.container = container;
+  this.items = this.container.children;
+
+  if (typeof MutationObserver !== "undefined") {
+    // Use the MutationObserver class to monitor changes to the dom element
+    var observer = new MutationObserver(this.calculateRows.bind(this));
+    observer.observe(container, {
+      attributes: true,
+      attributeFilter: ["class"]
+    });
+
+    // Also monitor for window resize events
+    window.addEventListener("resize", this.calculateRows.bind(this));
+
+    // Finally, just calculate everything RFN
+    this.calculateRows()
+  }
+
+}
+
+List.prototype.calculateRows = function(mutation) {
+  if (this.container.classList.contains("list--grid")) {
+    for (var i = 0; i < this.container.children.length; i++) {
+      var child = this.container.children[i];
+      this.calculateRowForItem(child);
+      imagesLoaded(child, this.calculateRowForItemOnImageLoad.bind(this));
+    }
+  }
+};
+
+List.prototype.calculateRowForItem = function(item) {
+  var rowHeight = parseInt(window.getComputedStyle(this.container).getPropertyValue("grid-auto-rows"));
+  var rowGap = parseInt(window.getComputedStyle(this.container).getPropertyValue("grid-row-gap"));
+  var rowSpan = Math.ceil((item.scrollHeight + rowGap) / (rowHeight + rowGap));
+  console.log(item.getBoundingClientRect(), rowHeight, rowGap, rowSpan);
+  item.style.gridRowEnd = "span "+ rowSpan;
+};
+
+List.prototype.calculateRowForItemOnImageLoad = function(loaded) {
+  console.log("loading again jeez");
+  this.calculateRowForItem(loaded.elements[0]);
+};
--- app/assets/javascripts/redesign.js
+++ app/assets/javascripts/redesign.js
@@ -2,14 +2,20 @@
 //= require_tree ./components
 //= require vendor/bootstrap-native-v4
 
-document.querySelectorAll(".segmented-control").forEach(function(control) {
-  var segmentedControl = new SegmentedControl(control);
-});
+(function() {
+  document.querySelectorAll(".list").forEach(function(control) {
+    var list = new List(control);
+  });
 
-document.querySelectorAll("[data-select-all]").forEach(function(control) {
-  var selectAll = new SelectAll(control);
-});
+  document.querySelectorAll(".segmented-control").forEach(function(control) {
+    var segmentedControl = new SegmentedControl(control);
+  });
 
-document.querySelectorAll("[data-toggle-target]").forEach(function(control) {
-  var toggle = new ToggleControl(control);
-});
+  document.querySelectorAll("[data-select-all]").forEach(function(control) {
+    var selectAll = new SelectAll(control);
+  });
+
+  document.querySelectorAll("[data-toggle-target]").forEach(function(control) {
+    var toggle = new ToggleControl(control);
+  });
+})();
