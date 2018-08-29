7aa0e35963788fc8d1b19763f8519cece99e703d
Flip Sasser <flip@inthebackforty.com>
2018-08-29 13:45:38 -0700

    Derp - answers

--- lib/scavenger_hunt/app/models/scavenger_hunt/answer.rb
+++ lib/scavenger_hunt/app/models/scavenger_hunt/answer.rb
@@ -3,14 +3,17 @@ class ScavengerHunt::Answer < ScavengerHunt::ApplicationRecord
   before_create :set_is_correct
 
   belongs_to :clue
-  belongs_to :resource
 
   scope :is_correct, -> { where(is_correct: true) }
 
   private
 
+  def clean(text)
+    text.downcase.gsub(/[^a-z0-9]/, "")
+  end
+
   def set_is_correct
-    self.is_correct = resource_id == clue.representation.resource_id
+    self.is_correct = clean(answer) == clean(clue.answer)
     clue.touch(:ended_at) if is_correct?
     true
   end
