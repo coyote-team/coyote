6fddea739ec24fb6b4322d4e1144660adcbb589a
Flip Sasser <flip@inthebackforty.com>
2018-09-26 14:34:16 -0700

    Rework survey page for accessibility reasons

--- lib/scavenger_hunt/app/controllers/scavenger_hunt/survey_questions_controller.rb
+++ lib/scavenger_hunt/app/controllers/scavenger_hunt/survey_questions_controller.rb
@@ -1,11 +1,12 @@
 class ScavengerHunt::SurveyQuestionsController < ScavengerHunt::ApplicationController
   def answer
-    survey_question = ScavengerHunt::SurveyQuestion.find(params[:id])
-    survey_question.answer(current_player, params[:answer])
+    params[:answers].each do |id, answer|
+      survey_question = ScavengerHunt::SurveyQuestion.find(id)
+      survey_question.answer(current_player, answer)
+    end
   end
 
   def index
     @survey_questions = ScavengerHunt::SurveyQuestion.all
   end
 end
-
