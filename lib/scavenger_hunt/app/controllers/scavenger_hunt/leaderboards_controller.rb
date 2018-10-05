e99e7d17451befc1b225f488be8323f9af7838a2
Flip Sasser <flip.sasser@gmail.com>
2018-10-05 16:07:15 -0700

    Sort leaderboard (#379)

--- lib/scavenger_hunt/app/controllers/scavenger_hunt/leaderboards_controller.rb
+++ lib/scavenger_hunt/app/controllers/scavenger_hunt/leaderboards_controller.rb
@@ -1,8 +1,5 @@
 class ScavengerHunt::LeaderboardsController < ScavengerHunt::ApplicationController
-
   def show
-    @players = ScavengerHunt::Player.where.not(name: nil).limit(10)
+    @players = ScavengerHunt::Player.where.not(name: nil).for_leaderboard.limit(10)
   end
-
 end
-
