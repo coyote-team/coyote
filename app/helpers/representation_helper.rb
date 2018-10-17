999143a5e422585d12f0e5eab4236ef95fa6eaed
Stacie Taylor-Cima <stacietaylorcima@Stacies-MacBook-Air.local>
2018-10-17 12:12:17 -0700

    make audit table into a list-like table displaying only auditable data

--- app/helpers/representation_helper.rb
+++ app/helpers/representation_helper.rb
@@ -11,4 +11,30 @@ module RepresentationHelper
       block_given? ? yield : representation.status.to_s.titleize
     end
   end
+
+  def audit_change_to_text(change)
+    old_value, new_value = change
+
+    if old_value.present? && new_value.nil?
+      %(#{old_value})
+    elsif old_value.present? && new_value.blank?
+      %(#{old_value} → Value was removed.)
+    else
+      %(#{old_value} → #{new_value})
+    end
+  end
+
+  def id_to_title(change, audit_class, audit_attribute)
+    old_value, new_value = change
+    old_title = audit_class.find_by(id: old_value)&.send(audit_attribute.to_sym)
+    new_title = audit_class.find_by(id: new_value)&.send(audit_attribute.to_sym)
+
+    if old_value.present? && new_value.nil?
+      old_title
+    elsif old_value.present? && new_value.blank?
+      old_title
+    else
+      %(#{old_title} → #{new_title})
+    end
+  end
 end
