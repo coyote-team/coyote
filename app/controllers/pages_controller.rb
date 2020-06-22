bf1b068c9fec9e34bdb7fe8d89c498d102dd0448
Flip Sasser <flip@inthebackforty.com>
2020-06-22 15:49:38 -0700

    Redirect users to intelligent locations instead of rendering an empty root
    Addresses #452

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,10 @@
 
 class PagesController < ApplicationController
   skip_before_action :require_user
+
+  def home
+    if current_user? && current_user.organizations.any?
+      redirect_to current_user.organizations.one? ? organization_path(current_user.organizations.first) : organizations_path
+    end
+  end
 end
--- spec/features/viewing_static_pages_spec.rb
+++ spec/features/viewing_static_pages_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe "Viewing static pages" do
     visit support_path
     expect(page).to have_current_path("/support")
   end
+
+  describe "with a logged-in user" do
+    let(:user) { create(:user, :with_membership, password: "password") }
+
+    before { login(user, "password") }
+
+    it "redirects to the user's sole organization at the root path" do
+      visit root_path
+      expect(page).to have_current_path(organization_path(user.organizations.reload.first))
+    end
+
+    it "redirects to the organization selector at the root path if the user has more than one organization" do
+      organization = create(:organization)
+      user.memberships.create!(organization: organization, role: :editor)
+      visit root_path
+      expect(page).to have_current_path(organizations_path)
+    end
+  end
 end
