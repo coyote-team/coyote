14a8db5a982200ef1d2ee59c1f98090bebabd9fd
Flip Sasser <flip@inthebackforty.com>
2020-06-25 08:10:02 -0700

    Assets cannot find application.css despite it being in the pipeline - removing HTML for now to get build running

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,19 +10,19 @@ assets.version = "1.0"
 
 # Add additional assets to the asset load path
 assets.paths << Rails.root.join("node_modules")
-assets.paths << Rails.root.join("app", "assets", "html")
+# assets.paths << Rails.root.join("app", "assets", "html")
 
 # Add support for Slim rendering of HTML files
-assets.configure do |env|
-  env.register_mime_type "text/slim", extensions: [".html.slim"]
-  env.register_transformer "text/slim", "text/html", StaticHTMLRenderer
-end
+# assets.configure do |env|
+#   env.register_mime_type "text/slim", extensions: [".html.slim"]
+#   env.register_transformer "text/slim", "text/html", StaticHTMLRenderer
+# end
 
 # Precompile additional assets
 assets.precompile += %w[application.css application.js]
 assets.precompile += %w[coyote_bookmarklet.js coyote_bookmarklet.css coyote_consumer.js coyote_producer.js]
 assets.precompile += %w[favicon/browserconfig.xml favicon/manifest.json]
-assets.precompile += %w[**/*.html]
+# assets.precompile += %w[**/*.html]
 
 # Prevent fingerprinting of generated HTML files, since they're used to render errors in production
-NonDigestAssets.whitelist += [%r{.+\.html$}] if defined? NonDigestAssets
+# NonDigestAssets.whitelist += [%r{.+\.html$}] if defined? NonDigestAssets
