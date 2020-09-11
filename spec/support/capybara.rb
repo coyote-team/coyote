9fc505c12ec18323eb97ea8ed3d8909e080c3067
Flip Sasser <flip.sasser@gmail.com>
2020-09-11 14:51:06 -0700

    Fix chrome driver issues in Docker

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -11,29 +11,31 @@ Capybara.match = :prefer_exact
 
 headless = :chrome_headless
 ui = :chrome
-options = {
+args = {
   browser: :chrome,
 }
 chromium_binary = `which chromium-browser`
-options[:binary] = chromium_binary if chromium_binary.present?
+args[:binary] = chromium_binary if chromium_binary.present?
 
 ## Headless Chrome (default)
 # Use a headless Chrome browser for TDD (when we don't want to be interrupted by the UI).
 # Add a `UI=1` before `bundle exec rspec` to prevent this from happening. We're using a custom
 # browser because we want to ensure the window size is large enough for testing in desktop mode.
 Capybara.register_driver headless do |app|
-  chrome_options = Capybara::Webmock.chrome_options
-  chrome_options.add_argument("headless")
-  chrome_options.add_argument("window-size=1280,800")
-  Capybara::Selenium::Driver.new(app, options.merge(options: chrome_options))
+  options = Capybara::Webmock.chrome_options
+  options.add_argument("disable-gpu")
+  options.add_argument("headless")
+  options.add_argument("no-sandbox")
+  options.add_argument("window-size=1280,800")
+  Capybara::Selenium::Driver.new(app, args.merge(options: options))
 end
 
 ## Chrome w/UI (optional)
 # Use a standard Chrome browser for tests where we'd like to watch the UI. Opt-in w/the `ui: true`
 # flag in your examples, or with a universal `UI=true bundle exec rspec` ENV flag.
 Capybara.register_driver ui do |app|
-  chrome_options = Capybara::Webmock.chrome_options
-  Capybara::Selenium::Driver.new(app, options.merge(options: chrome_options))
+  options = Capybara::Webmock.chrome_options
+  Capybara::Selenium::Driver.new(app, args.merge(options: options))
 end
 
 Capybara.javascript_driver = ENV["UI"] ? ui : headless
