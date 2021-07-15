aa27c99a41636d158e59493115fce3ba975e5057
Flip Sasser <flip.sasser@gmail.com>
2021-07-15 14:47:31 -0700

    Redis pool plz work

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
-redis_namespace = "#{Rails.env}-#{ENV["STAGING"] ? "staging" : Rails.env}"
+redis_namespace = "coyote-#{ENV["STAGING"] ? "staging" : Rails.env}"
+REDIS_POOL = ConnectionPool.new(size: 10) {
+  Redis.new(
+    connect_timeout:    1,
+    namespace:          redis_namespace,
+    reconnect_attempts: 3,
+    ssl_params:         {verify_mode: OpenSSL::SSL::VERIFY_NONE},
+    url:                ENV["REDIS_URL"],
+  )
+}
 
 Sidekiq.configure_server do |config|
-  config.redis = {
-    namespace: redis_namespace,
-  }
+  config.redis = REDIS_POOL
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {
-    namespace: redis_namespace,
-  }
+  config.redis = REDIS_POOL
 end
