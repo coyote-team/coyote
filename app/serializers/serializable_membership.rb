369796fadb9ff7fffdbbcfdd26624791a78e33c9
Job van Achterberg <mail@3ode.nl>
2022-01-27 14:55:36 +0100

    Include membership as part of serialized user response

--- app/serializers/serializable_membership.rb
+++ app/serializers/serializable_membership.rb
@@ -4,4 +4,9 @@ class SerializableMembership < JSONAPI::Serializable::Resource
   type "membership"
 
   attributes :active, :first_name, :last_name, :email, :role
+
+  attribute :organization_id do
+    # Map integer to string for response consistency
+    @object.organization_id.to_s
+  end
 end
